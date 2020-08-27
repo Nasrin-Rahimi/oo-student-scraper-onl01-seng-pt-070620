255152f1a0a14c0e050d442479c59d4e1f1b08c1
Nasrin Rahimi <77.nasrin@gmail.com>
2020-08-27 10:32:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,21 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    students = doc.css(".student-card")
+     binding.pry
+    # student_index_array = []
+    # students.each do |student|
+     
+    #   student_index_array <<  student.css(".student-name").text
+    #   # student_index_array << {"location" , student.css(".student-location").text}
+    #   # student_index_array[:url] = student.css("a")student.css("a")
+    # end
     
   end
 
